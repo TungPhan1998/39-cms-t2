96259c38e883c6beb1e93753e5d5edabff48c055
Phan Van Tung <phanvantung13579@gmail.com>
2019-10-28 05:49:12 +0700

    modul

---
+++ modul/doan/151/151-content.php
@@ -0,0 +1,26 @@
+<div class="content">
+    <div class="container">
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="social-icons">
+                    <div class="lang1"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            <a href="#"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></div>
+        © Porto eCommerce. 2019. All Rights Reserved</span>
+        <span class="lang2">© Porto eCommerce. 2019. All Rights Reserved</span></address></div>
+    </div>
+    <div id="back-top" class="hidden-phone" style="display: block;">
+    <a href="#top">
+      <div class="sticker-wrapper">
+        <div class="sticker">
+          <i class="icon-up-open"></i>
+        </div>
+      </div>
+    </a>
+  </div>
+    </div>
+</div>
+   
+    <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
